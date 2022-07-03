5deadf7864d01d3760933deaa4095ad83e530128
StarshipzZ <1915349279@qq.com>
2022-07-03 10:27:11 +0800

    redis环境的配置

---
+++ src/main/java/com/itheima/reggie/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.itheima.reggie.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author Hide on bush
+ * @date 2022/7/3 - 10:17
+ */
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        // 默认的key序列化器： JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
